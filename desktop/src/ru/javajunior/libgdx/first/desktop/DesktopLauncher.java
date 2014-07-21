382e68ee972a1f50001992f08e7c6312c1b6bf6d
komixx <komixx@mail.ru>
2014-07-21 18:25:09 +0400

    demo

---
+++ desktop/src/ru/javajunior/libgdx/first/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ru.javajunior.libgdx.first.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ru.javajunior.libgdx.first.Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Game(), config);
+	}
+}
