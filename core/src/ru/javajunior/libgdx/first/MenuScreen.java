a8cc6ab55b664bbd325b71647d51275cdf93eb34
komixx <komixx@mail.ru>
2014-08-24 18:56:16 +0400

    stage as InputProcessor

--- core/src/ru/javajunior/libgdx/first/MenuScreen.java
+++ core/src/ru/javajunior/libgdx/first/MenuScreen.java
@@ -1,24 +1,26 @@
 package ru.javajunior.libgdx.first;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.InputAdapter;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.scenes.scene2d.Stage;
 
 /**
  * Экран меню.
  */
 public class MenuScreen extends ScreenAdapter{
-    @Override
-    public void render(float delta) {
-        // Очистка экрана - вызов OpenGL функций.
-        Gdx.gl.glClearColor(1, 1, 0, 1);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-    }
-    @Override
-    public void show() {
-        // Устанавлиаем обработчик событий ввода
-        Gdx.input.setInputProcessor(new InputAdapter(){
+
+    /**
+     * Сцена. Содержит управляет всем что имеется на экране.
+     */
+    private Stage stage;
+
+    /**
+     * Создает и инициализирует экран меню
+     */
+    public MenuScreen() {
+        // Инициализирует сцену
+        this.stage = new Stage() {
             /** Вызывается при прикосновении к сенсорному экрану и нажатии на кнопку мыши.
              * Параметр button должен быть {@link com.badlogic.gdx.Input.Buttons#LEFT} для
              * Android и iOS.
@@ -33,6 +35,28 @@ public class MenuScreen extends ScreenAdapter{
                 Game.getInstance().showScreensaver();
                 return true;
             }
-        });
+        };
     }
+
+    /**
+     * Вызывается, когда экран становится акитвным.
+     */
+    @Override
+    public void show() {
+        // Устанавлиаем обработчик событий ввода
+        Gdx.input.setInputProcessor(stage);
+    }
+
+    /**
+     * Этот метод вызывается каждый кадр отрисовки.
+     * Частота его вызова непостоянна - FPS может "прыгать".
+     * @param delta - время в секундах с момента последней отрисовки.
+     */
+    @Override
+    public void render(float delta) {
+        // Очистка экрана - вызов OpenGL функций.
+        Gdx.gl.glClearColor(1, 1, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+    }
+
 }
